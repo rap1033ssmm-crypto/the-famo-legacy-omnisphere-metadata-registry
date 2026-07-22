e343efd07c0ef62850dff57661096734497db2fa
rap1033ssmm-crypto <rap1033ssmm@gmail.com>
2026-07-22 13:12:44 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'famo-core-v1';
+const ASSETS = [
+  'switchboard.html',
+  'logomatrix.html'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
